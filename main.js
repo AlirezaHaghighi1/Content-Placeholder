538d5466b3d09cad8a67ab3504af56b2a8301c97
Alireza Haghighi <alireza.haghighi1224@gmail.com>
2024-05-04 23:41:03 +0330

    typo fixed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const header = document.getElementById("header");
 const title = document.getElementById("title");
-const excerpt = documnet.getElementById("excerpt");
+const excerp = document.getElementById("excerp");
 const profile_img = document.getElementById("profile-img");
 const name = document.getElementById("name");
 const date = document.getElementById("date");
@@ -8,20 +8,18 @@ const date = document.getElementById("date");
 const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
 
-setTimeout(getData, 2500);
+setTimeout(getData, 2000);
 
 function getData() {
-  header.innerHTML = `<img src="/laptop-on-desk.jpg" alt="" />`;
-  title.innerHTML = `Lorem ipsum dolor sit amet.`;
-  excerpt.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-    at.`;
-  profile_img.innerHTML = `<img
-    src="https://randomuser.me/api/portraits/women/40.jpg"
-    alt="user"
-  />`;
+  header.innerHTML = '<img src="/laptop-on-desk.jpg" alt="" />';
+  title.innerHTML = "Lorem ipsum dolor sit amet.";
+  excerp.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/women/40.jpg" alt=""/>';
   name.innerHTML = "Max Calfield";
-  data.innerHTML = "Oct 08, 2020";
+  date.innerHTML = "Oct 08, 2020";
 
   animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
-  animated_bg_texts.forEach((bg) => bg.remove("animated-bg-text"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
 }
